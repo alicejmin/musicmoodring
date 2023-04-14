0fea08dcf4a36ef4dfae4931020f2cb9467308b3
mikaylawalsh <mikayla_walsh@brown.edu>
2023-04-14 09:25:10 -0400

    wip

---
+++ code/model.py
@@ -0,0 +1,92 @@
+from preprocess import get_data
+import tensorflow as tf
+import numpy as np
+
+
+class Model(tf.keras.Model):
+    def __init__(self):
+        super(Model, self).__init__()
+
+        # initialize layers and hyperparameters
+        # batch size
+        # number of classes (moods)
+        # learning rate
+        # epochs
+        # stride
+        # optimizer
+        # padding
+        self.batch_size = 32  # no idea
+        self.num_classes = 9
+        self.lr = .01
+        self.epochs = 10
+        # self.stride = (default is 1 so only need this if want something different?)
+        self.optimizer = tf.keras.optimizers.Adam(
+            learning_rate=self.lr)
+        self.padding = "SAME"
+        self.embedding_size = ?
+
+        # need embedding layer??
+
+        # look at paper for proper sizes and hypers
+
+        # 1D conv layer
+        # how should we determine hypers here
+        self.conv1d = tf.keras.layers.Conv1D(1, 3)
+        # max pool
+        self.max_pool = tf.keras.layers.MaxPool1D()
+        # LSTM
+        self.LSTM = tf.keras.layers.LSTM(self.embedding_size)
+        # Dropout
+        self.dropout = tf.keras.layers.Dropout(.5)
+        # dense
+        self.dense1 = tf.keras.layers.Dense()
+        # dropout
+        # dense
+        self.dense2 = tf.keras.layers.Dense()
+
+    def call(self, inputs):
+
+        # apply layers to inputs and return logits
+        pass
+
+    def loss(self, logits, labels):
+
+        # define a reasonable loss function
+        # (in the paper they use valance and arousal? maybe we pick something else)
+        pass
+
+    def accuracy(self, logits, labels):
+
+        # define a reasonable accuracy function
+        # (maybe different from paper)
+        pass
+
+
+def train(model, train_inputs, train_labels):
+
+    # POSSIBLE WE DO NOT NEED THIS -- CAN WE JUST USE .fit AND .evalutate IN MAIN?
+
+    # train model -- maybe shuffle inputs -- look at hw2 and hw3
+    # return average accuracy? maybe loss too (looking at one epoc only)
+    pass
+
+
+def test(model, test_inputs, test_labels):
+
+    # POSSIBLE WE DO NOT NEED THIS -- CAN WE JUST USE .fit AND .evalutate IN MAIN?
+
+    # Tests the model on the test inputs and labels.
+    # return average accuracy?
+    pass
+
+
+def main():
+    # read in data using get_data()
+    # initilize model
+    # train and test model for set number of epochs
+
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/preprocess.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import numpy as np
+import pickle
+
+def get_data(file_path, first_class, second_class):
+    pass
