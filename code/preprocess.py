a97e988f7b20cf5218676ee5f830ba495be14697
mikaylawalsh <mikayla_walsh@brown.edu>
2023-04-18 22:25:43 -0400

    wip

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,6 +1,27 @@
 import tensorflow as tf
 import numpy as np
 import pickle
+import csv
 
-def get_data(file_path, first_class, second_class):
-    pass
+
+def get_data(file_path):
+    # read in data
+    with open(file_path) as csvfile:
+        data = csv.reader(csvfile)
+
+    lyrics = data['lyrics']  # or maybe change to index?
+    labels = data['label']  # or maybe change to index?
+    # split into labels and lyrics
+
+    # total = 1160, 80% = 928, 20% = 232
+
+    labels = tf.one_hot(labels, 9)  # not sure if this is right -- 9 classes?
+
+    train_lyrics, test_lyrics = lyrics[:928], lyrics[929:]
+    train_labels, test_labels = labels[:928], labels[929:]
+
+    # already in 1-D arrays so don't need to flatten
+
+    # need to reshape?? normalize? one hot encode?
+
+    return tf.convert_to_tensor(train_lyrics), tf.convert_to_tensor(test_lyrics), tf.convert_to_tensor(train_labels), tf.convert_to_tensor(test_labels)
