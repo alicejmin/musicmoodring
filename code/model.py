8f7dc21b08e641e01d267131e8814505b40905a0
mikaylawalsh <mikayla_walsh@brown.edu>
2023-04-26 12:46:43 -0400

    wip

--- code/model.py
+++ code/model.py
@@ -23,9 +23,9 @@ class Model(tf.keras.Model):
         self.optimizer = tf.keras.optimizers.Adam(
             learning_rate=self.lr)
         self.padding = "SAME"
-        self.embedding_size = None #need to change later
-        self.vocab_size = None # pick a number 
-        self.hidden_size = None #need to change later
+        self.embedding_size = 64 #need to change later
+        self.vocab_size = 128 # pick a number 
+        self.hidden_size = 256 #need to change later
         
 
         # need embedding layer??
@@ -38,7 +38,7 @@ class Model(tf.keras.Model):
         self.conv1d = tf.keras.layers.Conv1D(1, 3, strides=1, padding=self.padding)
         # max pool
         # self.max_pool = tf.keras.layers.MaxPool1D() #or 
-        self.max_pool = tf.nn.max_pool(self.conv1d, [3, 3], strides=1, padding=self.padding)
+        self.max_pool = tf.nn.max_pool(self.conv1d, [3, 3], strides=1, padding=self.padding) # input?? change -- should not be self.conv1d
         # LSTM
         self.LSTM = tf.keras.layers.LSTM(self.embedding_size, activation="leaky_relu")
         # Dropout
@@ -78,7 +78,7 @@ class Model(tf.keras.Model):
 
 
 
-def train(model, train_inputs, train_labels):
+def train(model, train_lyrics, train_labels):
 
     # POSSIBLE WE DO NOT NEED THIS -- CAN WE JUST USE .fit AND .evalutate IN MAIN?
     
@@ -87,10 +87,31 @@ def train(model, train_inputs, train_labels):
 
     # train model -- maybe shuffle inputs -- look at hw2 and hw3
     # return average accuracy? maybe loss too (looking at one epoc only)
-    pass
+    avg_acc = 0
+    counter = 0
+    for batch_num, b1 in enumerate(range(model.batch_size, train_lyrics.shape[0] + 1, model.batch_size)):
+        b0 = b1 - model.batch_size
+        batch_lyrics = train_lyrics[b0:b1]
+        batch_labels = train_labels[b0:b1]
+
+        with tf.GradientTape() as tape:
+                logits = model(batch_lyrics) 
+                loss = model.loss(batch_labels, logits)
+        
+        grads = tape.gradient(loss, model.trainable_variables)
+        model.optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        acc = model.accuracy(logits, batch_labels)
+        avg_acc += acc
+        counter += 1
+        print("TRAIN", "batch:", batch_num, "acc:", acc)
+        print("TRAIN", "batch:", batch_num, "loss:", loss)
+    
+    print("average accuracy:", avg_acc/counter)
+
+    return
 
 
-def test(model, test_inputs, test_labels):
+def test(model, test_lyrics, test_labels):
 
     # POSSIBLE WE DO NOT NEED THIS -- CAN WE JUST USE .fit AND .evalutate IN MAIN?
 
@@ -98,13 +119,42 @@ def test(model, test_inputs, test_labels):
 
     # Tests the model on the test inputs and labels.
     # return average accuracy?
-    pass
+
+    avg_acc = 0
+    counter = 0
+    for batch_num, b1 in enumerate(range(model.batch_size, test_lyrics.shape[0] + 1, model.batch_size)):
+        b0 = b1 - model.batch_size
+        batch_lyrics = test_lyrics[b0:b1]
+        batch_labels = test_labels[b0:b1]
+
+        logits = model(batch_lyrics)
+        loss = model.loss(batch_labels, logits)
+
+        acc = model.accuracy(logits, batch_labels)
+        avg_acc += acc
+        counter += 1
+        print("TEST", "batch:", batch_num, "acc:", acc)
+        print("TEST", "batch:", batch_num, "loss:", loss)
+
+    return avg_acc/counter
 
 
 def main():
     # read in data using get_data()
     # initilize model
     # train and test model for set number of epochs
+
+    train_lyrics, test_lyrics, train_labels, test_labels = get_data(
+        "data/singlelabel.csv")
+    
+    model = Model()
+
+    for _ in range(model.epochs):
+        train(model, train_lyrics, train_labels)
+
+    t = test(model, test_lyrics, test_labels)
+    
+    print("FINAL TEST ACC:", t)
 
     return
 
