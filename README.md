c54f468832de1af1d90c8103ad30ca6a0ac5b66b
lgramley <lucy_gramley@brown.edu>
2023-05-07 18:42:54 -0400

    added comments --> cleaned

--- README.md
+++ README.md
@@ -5,4 +5,11 @@ The original paper uses lyrics as well as audio, however, we are simplifying thi
 The applications of this project would be used to do something such as making playlists on spotify (or other music platform) or radio genres. 
 Ultimately, it would help users find music in a mood category of their choosing.
 
-See our paper for more information!
+Our loss/accuracy:
+For classification(model_mood.py): loss: .979; accuracy: .536
+For regression(model_val.py): loss: .037; r_squared: .134
+
+See our paper for more information and detailed results! 
+
+Please note: Our larger dataset is too large to upload to git. Please download it yourself and you will be able to run our code. Here is the link:
+ https://www.kaggle.com/datasets/edenbd/150k-lyrics-labeled-with-spotify-valence?resource=download.
--- code/preprocess_mood.py
+++ code/preprocess_mood.py
@@ -16,8 +16,8 @@ def get_data(file_path):
     lyrics = data['lyrics']
     labels = data['label']
 
-    # IF WE WANT EACH SET OF LYRICS TO HAVE ITS OWN LIST
-
+    # Gives each set of lyrics its own list
+    # clean the data
     stop_words = set(stopwords.words('english'))
 
     lyrics = [[word.lower().strip("!()-',.?*{};:¡\"“‘~…’—–”\\")
@@ -29,64 +29,24 @@ def get_data(file_path):
     upper_bound = mean + 2*std
     lower_bound = mean - 2*std
 
+    # helps with length of song/filtering out bad data
     indices = np.nonzero([1 if len(song) <= upper_bound and len(
         song) >= lower_bound else 0 for song in lyrics])[0]
 
     lyrics = [lyrics[i] for i in indices]
     labels = [labels[i] for i in indices]
 
-    # lyrics_sad = [lyrics[i] for i in range(len(labels)) if labels[i] == "Sadness"]
-    # labels_sad = [labels[i] for i in range(len(labels)) if labels[i] == "Sadness"]
-
-    # lyrics_tender = [lyrics[i] for i in range(len(labels)) if labels[i] == "Tenderness"]
-    # labels_tender = [labels[i] for i in range(len(labels)) if labels[i] == "Tenderness"]
-
-    # lyrics_tension = [lyrics[i] for i in range(len(labels)) if labels[i] == "Tension"]
-    # labels_tension = [labels[i] for i in range(len(labels)) if labels[i] == "Tension"]
-
-    # lyrics_sad = lyrics_sad[:267]
-    # labels_sad = labels_sad[:267]
-    # # print(len(lyrics_sad))
-
-    # lyrics_tender = lyrics_tender[:267]
-    # labels_tender = labels_tender[:267]
-    # # print(len(lyrics_tender))
-
-    # # print(len(lyrics_tension))
-    # lyrics = np.concatenate((lyrics_sad, lyrics_tender, lyrics_tension))
-    # labels = np.concatenate((labels_sad, labels_tender, labels_tension))
-    # print(len(lyrics))
+    # shortens/evens out the list of lyrics
     for song in range(len(lyrics)):
         lyrics[song] = lyrics[song][:50]
-    
+
+    # assigns each label a number --> one hot encodes labels
     indices = [0 if x == 'Tension' else 1 if x ==
                'Tenderness' else 2 for x in labels]
-    
-    # tot = list(zip(lyrics, labels))
-    # sad = 0
-    # tender = 0 
-    # tension = 0
-    # for i in tot:
-    #     if i[1] == 0 and sad > 300:
-    #         sad +=1
-    #         tot.remove(i)
-    #     elif i[1] ==1 and tension > 300:
-    #         tension +=1
-    #         tot.remove(i)
-    #     elif i[1] == 2 and tender > 200: 
-    #         tender +=1
-    #         tot.remove(i)
-    #     elif i[1] == 0:
-    #         sad +=1
-    #     elif i[1] ==1:
-    #         tension +=1
-    #     else: 
-    #         tender +=1
-    # lyrics, indices = zip(*tot)
 
     labels = tf.one_hot(indices, 3, dtype=tf.int64)
 
-    # IF WE WANT ONE LIST FOR ALL LYRICS
+    # If we want one list of all lyrics:
     # train_lyrics_list = []
     # for x in train_lyrics:
     #     train_lyrics_list.append(x.split())
@@ -94,6 +54,7 @@ def get_data(file_path):
     # for y in test_lyrics:
     #     test_lyrics_list.append(y.split())
 
+    # gets rid of duplicate data
     unique = []
     for song in lyrics:
         unique.extend(song)
@@ -104,37 +65,33 @@ def get_data(file_path):
     lyrics = [list(map(lambda x: vocabulary[x], song))
               for song in lyrics]
 
+    # pads data so it can be converted to tensor eventually and for even training
     lyrics = tf.keras.preprocessing.sequence.pad_sequences(
         lyrics, padding='post')  # returns np array
-    # singlelabel:
+
+    # singlelabel (math):
     # total = 1103, 80% = 882, 20% = 221
     # labeled_lyrics
     # total = 150568, 80% = 120,454, 20% = 30,114
 
-    # 827
-
+    # batch data
     index_range = tf.random.shuffle(range(len(lyrics)))
     shuffled_lyrics = tf.gather(lyrics, index_range)
     shuffled_labels = tf.gather(labels, index_range)
 
     train_lyrics, test_lyrics = shuffled_lyrics[:882], shuffled_lyrics[882:]
     train_labels, test_labels = shuffled_labels[:882], shuffled_labels[882:]
-    # print(train_labels)
-    # print(test_labels)
 
+    # convert everything to a tensor and return data
     return tf.convert_to_tensor(train_lyrics), tf.convert_to_tensor(test_lyrics), train_labels, test_labels
 
 
 def main():
-    # can delete later -- just for testing
+    # just for testing
+    # add print statements as necessary to test for proper output
 
     X0, Y0, X1, Y1 = get_data(
         "data/singlelabel.csv")
-
-    #print(X0)
-    # print(Y0)
-    #print(X1)
-    # print(Y1)
 
     return
 
